bb54a329dab31df02dc0de4789f944f4896a3130
Abdullah AlHabal <dbnkalhbalb@gmail.com>
2023-12-24 14:13:08 +0300

    Create the Resources[view] files for department-employee with Blade and layouts - add the routes

--- Employee and Department Management System Assessment/routes/web.php
+++ Employee and Department Management System Assessment/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\DepartmentController;
+use App\Http\Controllers\EmployeeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +16,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('department.index');
 });
+
+Route::resource('department', DepartmentController::class);
+
+
+Route::resource('employee', EmployeeController::class);
